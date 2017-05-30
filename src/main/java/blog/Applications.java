6094911c832169279855c8654193de3549526b5d
gmx9308 <gmx9308@gmail.com>
2017-05-29 21:01:10 -0700

    create home page

---
+++ src/main/java/blog/Applications.java
@@ -0,0 +1,12 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applications {
+	public static void main(String[] args){
+		SpringApplication.run(Applications.class, args);
+		
+	}
+}
