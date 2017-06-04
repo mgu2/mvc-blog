3911dc36d19056b04b1c43a65a2ee292e7c33894
gmx9308 <gmx9308@gmail.com>
2017-06-03 22:16:29 -0700

    add single id's view API

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import blog.models.Post;
@@ -26,5 +27,16 @@ public class HomeController {
 		.limit(3).collect(Collectors.toList());
 		model.addAttribute("latest3posts", latest3Posts);
 		return "index";
+	}
+	
+	@RequestMapping("posts/views/{id}")
+	public String view(
+			@PathVariable("id") Long id,
+			Model model
+			){
+		Post post = postService.findById(id);
+		model.addAttribute("post", post);
+		return "posts/view";
+		
 	}
 }
