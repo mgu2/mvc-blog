6ed0ff5cabeb7f9f661169aec8e0a2b7422bf343
gmx9308 <gmx9308@gmail.com>
2017-06-08 11:13:31 -0700

    add delete and edit posts API

--- src/main/java/blog/controllers/PostsController.java
+++ src/main/java/blog/controllers/PostsController.java
@@ -1,10 +1,15 @@
 package blog.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import blog.models.Post;
 import blog.services.NotificationService;
@@ -43,4 +48,37 @@ public class PostsController {
         		
             return "redirect:/";
         }
+        
+        @RequestMapping(value="/posts/create", method=RequestMethod.POST)
+        public String create(
+        		@RequestBody @Valid Post post, 
+        		Model model){
+        	
+        	if(post==null)
+        		notifyService.addErrorMessage("Cannot create new post");
+        	else{
+        		postService.create(post);
+        		notifyService.addInfoMessage("Post created successfully");
+        	}
+        	
+        	return "redirect:/";
+        }
+        
+        @RequestMapping(value="/post/edit/{id}", method=RequestMethod.PATCH)
+        public String edit(
+        		@RequestParam(required = true) Long id,
+        		@RequestBody @Valid Post post,
+        		Model model){
+        	
+        	if(post==null||postService.findById(id)==null)
+        		notifyService.addErrorMessage("Cannot create new post");
+        	
+        	postService.deleteById(id);
+        	postService.create(post);
+        	
+        	
+        	return "redirect:/";
+        }
+        
+        
 }
