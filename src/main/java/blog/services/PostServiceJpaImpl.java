5ea4558285301f9622aaaab82da910547840ef53
gmx9308 <gmx9308@gmail.com>
2017-06-08 11:54:54 -0700

    rewrite model and service in Jpa Annotations

--- src/main/java/blog/services/PostServiceJpaImpl.java
+++ src/main/java/blog/services/PostServiceJpaImpl.java
@@ -2,13 +2,23 @@ package blog.services;
 
 import blog.models.Post;
 import blog.models.User;
+import blog.repositories.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
-public class PostServiceStubImpl implements PostService {
+@Primary
+public class PostServiceJpaImpl implements PostService {
+	
+	@Autowired
+	private PostRepository postRepo;
+	
     private List<Post> posts = new ArrayList<Post>() {{
         add(new Post(1L, "First Post", "<p>Line #1.</p><p>Line #2</p>", null));
         add(new Post(2L, "Second Post", "Second post content:<ul><li>line 1</li><li>line 2</li></p>", new User(10L, "pesho10", "Peter Ivanov")));
@@ -20,51 +30,27 @@ public class PostServiceStubImpl implements PostService {
 
     @Override
     public List<Post> findAll() {
-        return this.posts;
+    return this.postRepo.findAll();
     }
 
     @Override
     public List<Post> findLatest5() {
-        return  this.posts.stream()
-                .sorted((a, b) -> b.getDate().compareTo(a.getDate()))
-                .limit(5).collect(Collectors.toList());
+    return this.postRepo.findLatest5Posts(new PageRequest(0, 5));
     }
-
     @Override
     public Post findById(Long id) {
-        return this.posts.stream()
-                .filter(p -> Objects.equals(p.getId(), id))
-                .findFirst()
-                .orElse(null);
+    return this.postRepo.findOne(id);
     }
-
     @Override
     public Post create(Post post) {
-        post.setId(this.posts.stream().mapToLong(
-                p -> p.getId()).max().getAsLong() + 1);
-        this.posts.add(post);
-        return post;
+    return this.postRepo.save(post);
     }
-
     @Override
     public Post edit(Post post) {
-        for (int i = 0; i < this.posts.size(); i++) {
-            if (Objects.equals(this.posts.get(i).getId(), post.getId())) {
-                this.posts.set(i, post);
-                return post;
-            }
-        }
-        throw new RuntimeException("Post not found: " + post.getId());
+    return this.postRepo.save(post);
     }
-
     @Override
     public void deleteById(Long id) {
-        for (int i = 0; i < this.posts.size(); i++) {
-            if (Objects.equals(this.posts.get(i).getId(), id)) {
-                this.posts.remove(i);
-                return;
-            }
-        }
-        throw new RuntimeException("Post not found: " + id);
+    this.postRepo.delete(id);
     }
 }
